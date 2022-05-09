c02b61a592443b7bfb75fa088372d7eba424ea0b
Priya Aswani <priya.aswani28139@gmail.com>
2022-05-09 20:28:16 +0400

    Wrap FoodEntryList in Paper

--- src/components/FoodEntryList.js
+++ src/components/FoodEntryList.js
@@ -7,6 +7,7 @@ import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
 import { Box } from "@mui/system";
 import { FoodEntry } from "./index";
+import { Paper, Typography } from "@mui/material";
 
 export const FoodEntryList = () => {
   const [foodEntries, setFoodEntries] = useState([
@@ -43,31 +44,37 @@ export const FoodEntryList = () => {
   ]);
 
   return (
-    <TableContainer>
-      <Box sx={{ maxWidth: 800, px: 5, mx: "auto" }}>
-        <Table aria-label="simple table" size="small">
-          <TableHead>
-            <TableRow>
-              <TableCell component="th">Index</TableCell>
-              <TableCell component="th" align="center">
-                Product name
-              </TableCell>
-              <TableCell component="th" align="center">
-                Calories
-              </TableCell>
-              <TableCell component="th" align="center">
-                Consumed at
-              </TableCell>
-              <TableCell component="th" align="center"></TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {foodEntries.map((foodEntry) => (
-              <FoodEntry {...foodEntry} key={foodEntry.id.toString()} />
-            ))}
-          </TableBody>
-        </Table>
-      </Box>
-    </TableContainer>
+    <Paper sx={{ mx: 4, textAlign: "center", pt: 3, pb: 5 }}>
+      <Typography variant="h3" component="h1" sx={{ mb: 4, fontWeight: 100 }}>
+        Your current food entries :)
+      </Typography>
+
+      <TableContainer>
+        <Box sx={{ maxWidth: 800, px: 5, mx: "auto" }}>
+          <Table aria-label="simple table" size="small">
+            <TableHead>
+              <TableRow>
+                <TableCell component="th">Index</TableCell>
+                <TableCell component="th" align="center">
+                  Product name
+                </TableCell>
+                <TableCell component="th" align="center">
+                  Calories
+                </TableCell>
+                <TableCell component="th" align="center">
+                  Consumed at
+                </TableCell>
+                <TableCell component="th" align="center"></TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {foodEntries.map((foodEntry) => (
+                <FoodEntry {...foodEntry} key={foodEntry.id.toString()} />
+              ))}
+            </TableBody>
+          </Table>
+        </Box>
+      </TableContainer>
+    </Paper>
   );
 };
