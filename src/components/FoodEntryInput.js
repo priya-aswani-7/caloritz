aaf5bcd89b28fd9ae3da9382e9b75feb5a90edb0
Priya Aswani <priya.aswani28139@gmail.com>
2022-05-09 23:09:22 +0400

    Allow toggling form display in FoodEntryInput

--- src/components/FoodEntryInput.js
+++ src/components/FoodEntryInput.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { Button, Fab, Paper, TextField, Typography } from "@mui/material";
 import AddIcon from "@mui/icons-material/Add";
 import ClearIcon from "@mui/icons-material/Clear";
@@ -9,11 +10,16 @@ import { DateTimePicker } from "@mui/x-date-pickers/DateTimePicker";
 import Grid from "@mui/material/Grid";
 
 export const FoodEntryInput = () => {
+  const [displayForm, setDisplayForm] = useState(null);
   return (
     <Box sx={{ mb: 3 }}>
       <Box sx={{ textAlign: "center" }}>
-        <Fab color="primary" aria-label="add">
-          <AddIcon />
+        <Fab
+          color={displayForm ? "error" : "warning"}
+          aria-label="add"
+          onClick={() => setDisplayForm(!displayForm)}
+        >
+          {displayForm ? <ClearIcon /> : <AddIcon />}
         </Fab>
         <Typography
           variant="subtitle1"
@@ -21,59 +27,63 @@ export const FoodEntryInput = () => {
           gutterBottom
           component="div"
         >
-          Create a new food entry
+          {displayForm
+            ? "No thanks, I'll add a food entry later"
+            : "Create a new food entry"}
         </Typography>
       </Box>
-      <Box sx={{ display: "flex", justifyContent: "center" }}>
-        <Paper
-          sx={{
-            maxWidth: 800,
-            mx: 4,
-            p: 3,
-          }}
-        >
-          <Grid container spacing={2} mb={3}>
-            <Grid item xs={12} sm={12}>
-              <TextField label="Product Name" variant="outlined" fullWidth />
-            </Grid>
-            <Grid item xs={6} sm={6}>
-              <TextField
-                label="Calories"
-                type="Number"
-                variant="outlined"
-                fullWidth
-              />
-            </Grid>
-            <Grid item xs={6} md={6}>
-              <LocalizationProvider dateAdapter={AdapterDateFns}>
-                <DateTimePicker
-                  renderInput={(props) => <TextField {...props} fullWidth />}
-                  label="Consumed At"
-                />
-              </LocalizationProvider>
-            </Grid>
-          </Grid>
-
-          <Box
+      {displayForm && (
+        <Box sx={{ display: "flex", justifyContent: "center" }}>
+          <Paper
             sx={{
-              display: "flex",
-              justifyContent: "flex-end",
+              maxWidth: 800,
+              mx: 4,
+              p: 3,
             }}
           >
-            <Button
-              variant="outlined"
-              sx={{ mr: 1 }}
-              size="large"
-              startIcon={<ClearIcon />}
+            <Grid container spacing={2} mb={3}>
+              <Grid item xs={12} sm={12}>
+                <TextField label="Product Name" variant="outlined" fullWidth />
+              </Grid>
+              <Grid item xs={6} sm={6}>
+                <TextField
+                  label="Calories"
+                  type="Number"
+                  variant="outlined"
+                  fullWidth
+                />
+              </Grid>
+              <Grid item xs={6} md={6}>
+                <LocalizationProvider dateAdapter={AdapterDateFns}>
+                  <DateTimePicker
+                    renderInput={(props) => <TextField {...props} fullWidth />}
+                    label="Consumed At"
+                  />
+                </LocalizationProvider>
+              </Grid>
+            </Grid>
+
+            <Box
+              sx={{
+                display: "flex",
+                justifyContent: "flex-end",
+              }}
             >
-              Clear
-            </Button>
-            <Button variant="contained" size="large" endIcon={<SendIcon />}>
-              Save
-            </Button>
-          </Box>
-        </Paper>
-      </Box>
+              <Button
+                variant="outlined"
+                sx={{ mr: 1 }}
+                size="large"
+                startIcon={<ClearIcon />}
+              >
+                Clear
+              </Button>
+              <Button variant="contained" size="large" endIcon={<SendIcon />}>
+                Save
+              </Button>
+            </Box>
+          </Paper>
+        </Box>
+      )}
     </Box>
   );
 };
