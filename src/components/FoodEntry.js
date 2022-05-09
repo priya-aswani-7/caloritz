610e3308af0fcaadb1c217b49afa2039c298ab49
Priya Aswani <priya.aswani28139@gmail.com>
2022-05-09 14:51:23 +0400

    Add more options icon to each FoodEntry row

--- src/components/FoodEntry.js
+++ src/components/FoodEntry.js
@@ -1,5 +1,8 @@
 import TableCell from "@mui/material/TableCell";
 import TableRow from "@mui/material/TableRow";
+import IconButton from "@mui/material/IconButton";
+import MoreVertIcon from "@mui/icons-material/MoreVert";
+import { grey } from "@mui/material/colors";
 
 export const FoodEntry = ({ id, productName, calories, consumedAt }) => {
   return (
@@ -8,6 +11,11 @@ export const FoodEntry = ({ id, productName, calories, consumedAt }) => {
       <TableCell align="center">{productName}</TableCell>
       <TableCell align="center">{calories}</TableCell>
       <TableCell align="center">{consumedAt}</TableCell>
+      <TableCell align="center">
+        <IconButton component="span">
+          <MoreVertIcon sx={{ color: grey[500] }} />
+        </IconButton>
+      </TableCell>
     </TableRow>
   );
 };
