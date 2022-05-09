8c628402f6f4d7809996f01fef2d4f200123354b
Priya Aswani <priya.aswani28139@gmail.com>
2022-05-10 01:28:59 +0400

    Refactor render of FoodEntry based off new data structure for foodEntries

--- src/components/FoodEntry.js
+++ src/components/FoodEntry.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import TableCell from "@mui/material/TableCell";
 import TableRow from "@mui/material/TableRow";
 import MoreVertIcon from "@mui/icons-material/MoreVert";
@@ -7,6 +7,7 @@ import { Button, Popover, Typography } from "@mui/material";
 
 export const FoodEntry = ({ id, productName, calories, consumedAt }) => {
   const [anchorEl, setAnchorEl] = useState(null);
+  const [consumedAtTimeString, setConsumedAtTimeString] = useState(null);
   const open = Boolean(anchorEl);
   const popOverId = open ? "simple-popover" : undefined;
 
@@ -18,12 +19,40 @@ export const FoodEntry = ({ id, productName, calories, consumedAt }) => {
     setAnchorEl(null);
   };
 
+  useEffect(() => {
+    let hours = consumedAt.getHours();
+    let minutes = consumedAt.getMinutes();
+    let meridian = hours > 12 || (hours === 12 && minutes > 0) ? "pm" : "am";
+    let dateString = consumedAt.toString().split(" ");
+
+    let string =
+      (meridian === "pm"
+        ? hours < 22
+          ? "0" + hours - 12
+          : hours - 12
+        : hours < 10
+        ? "0" + hours
+        : hours) +
+      ":" +
+      (minutes < 10 ? "0" + minutes : minutes) +
+      " " +
+      meridian +
+      ", " +
+      dateString[2] +
+      " " +
+      dateString[1] +
+      " " +
+      dateString[3];
+
+    setConsumedAtTimeString(string);
+  }, [consumedAt]);
+
   return (
     <TableRow>
       <TableCell>{id}</TableCell>
       <TableCell align="center">{productName}</TableCell>
       <TableCell align="center">{calories}</TableCell>
-      <TableCell align="center">{consumedAt}</TableCell>
+      <TableCell align="center">{consumedAtTimeString}</TableCell>
       <TableCell align="right">
         <Button onClick={handleClick}>
           <MoreVertIcon sx={{ color: grey[500] }} />
