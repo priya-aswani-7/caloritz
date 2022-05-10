21c7cbba13152029f50e223ba768dccd1c296791
Priya Aswani <priya.aswani28139@gmail.com>
2022-05-10 20:57:51 +0400

    Cleanup structure for foodEntries

--- src/App.js
+++ src/App.js
@@ -4,35 +4,30 @@ import { NavBar, FoodEntryList, FoodEntryInput } from "./components";
 export const App = () => {
   const [foodEntries, setFoodEntries] = useState([
     {
-      id: 1,
       productName: "Frozen yoghurt",
       cost: 20.12,
       calories: 159,
       consumedAt: new Date("10:02 pm, 22 April 2022"),
     },
     {
-      id: 2,
       productName: "Ice cream sandwich",
       cost: 34.09,
       calories: 237,
       consumedAt: new Date("08:02 am, 22 April 2022"),
     },
     {
-      id: 3,
       productName: "Eclair",
       cost: 45.32,
       calories: 262,
       consumedAt: new Date("05:00 pm, 21 April 2022"),
     },
     {
-      id: 4,
       productName: "Cupcake",
       cost: 22.34,
       calories: 305,
       consumedAt: new Date("11:03 pm, 20 April 2022"),
     },
     {
-      id: 5,
       productName: "Gingerbread",
       cost: 11.34,
       calories: 356,
