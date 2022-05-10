b3b01d1c5801a171700895dcc61b0b774c460a99
Priya Aswani <priya.aswani28139@gmail.com>
2022-05-10 16:07:19 +0400

    Refactor view of FoodEntryInput using Form Dialog

--- src/components/FoodEntryInput.js
+++ src/components/FoodEntryInput.js
@@ -1,37 +1,22 @@
-import { useEffect, useState } from "react";
-import {
-  FormControl,
-  Button,
-  Fab,
-  InputLabel,
-  OutlinedInput,
-  Paper,
-  TextField,
-  Typography,
-  InputAdornment,
-} from "@mui/material";
-import AddIcon from "@mui/icons-material/Add";
-import ClearIcon from "@mui/icons-material/Clear";
-import SendIcon from "@mui/icons-material/Send";
-import { Box } from "@mui/system";
+import { useState } from "react";
+import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogContentText from "@mui/material/DialogContentText";
+import DialogTitle from "@mui/material/DialogTitle";
+import { Box, Grid } from "@mui/material";
 import { AdapterDateFns } from "@mui/x-date-pickers/AdapterDateFns";
-import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
-import { DateTimePicker } from "@mui/x-date-pickers/DateTimePicker";
-import Grid from "@mui/material/Grid";
+import { DateTimePicker, LocalizationProvider } from "@mui/x-date-pickers";
 import { cloneDeep } from "lodash";
 
 export const FoodEntryInput = ({ foodEntries, setFoodEntries }) => {
-  const [displayForm, setDisplayForm] = useState(null);
+  const [open, setOpen] = useState(false);
   const [productName, setProductName] = useState("");
   const [cost, setCost] = useState("");
   const [calories, setCalories] = useState("");
   const [consumedAt, setConsumedAt] = useState(null);
-
-  const handleClear = () => {
-    setProductName("");
-    setCalories("");
-    setConsumedAt(null);
-  };
 
   const handleSave = () => {
     const currentFoodEntries = foodEntries ? cloneDeep(foodEntries) : [];
@@ -44,133 +29,99 @@ export const FoodEntryInput = ({ foodEntries, setFoodEntries }) => {
     });
 
     setFoodEntries(currentFoodEntries);
-    setDisplayForm(false);
-    handleClear();
+
+    setProductName("");
+    setCalories("");
+    setCost("");
+    setConsumedAt(null);
+    setOpen(false);
   };
 
-  useEffect(() => {
-    console.log("pn changed:", productName);
-  }, [productName]);
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
 
-  useEffect(() => {
-    console.log("calories changed:", calories);
-  }, [calories]);
-
-  useEffect(() => {
-    console.log("consumedAt changed:", consumedAt);
-  }, [consumedAt]);
+  const handleClose = () => {
+    setOpen(false);
+  };
 
   return (
-    <Box sx={{ mb: 3 }}>
-      <Box sx={{ textAlign: "center" }}>
-        <Fab
-          color={displayForm ? "error" : "warning"}
-          aria-label="add"
-          onClick={() => setDisplayForm(!displayForm)}
-        >
-          {displayForm ? <ClearIcon /> : <AddIcon />}
-        </Fab>
-        <Typography
-          variant="subtitle1"
-          sx={{ fontWeight: 100, mt: 1 }}
-          gutterBottom
-          component="div"
-        >
-          {displayForm
-            ? "No thanks, I'll add a food entry later"
-            : "Create a new food entry"}
-        </Typography>
-      </Box>
-      {displayForm && (
-        <Box sx={{ display: "flex", justifyContent: "center" }}>
-          <Paper
-            sx={{
-              maxWidth: 800,
-              mx: 4,
-              p: 3,
-            }}
-          >
-            <Grid container spacing={2} mb={3}>
-              <Grid item xs={12} sm={8}>
-                <TextField
-                  label="Product Name"
-                  variant="outlined"
-                  value={productName}
-                  onChange={(event) => setProductName(event.target.value)}
-                  fullWidth
-                  required
-                />
-              </Grid>
-              <Grid item xs={12} sm={4}>
-                <FormControl fullWidth required>
-                  <InputLabel>Cost</InputLabel>
-                  <OutlinedInput
-                    type="Number"
-                    value={cost}
-                    onChange={(event) =>
-                      setCost(parseFloat(event.target.value) || "")
-                    }
-                    startAdornment={
-                      <InputAdornment position="start">$</InputAdornment>
-                    }
-                    label="Cost"
-                  />
-                </FormControl>
-              </Grid>
-              <Grid item xs={12} sm={6}>
-                <TextField
-                  label="Calories"
-                  type="Number"
-                  value={calories}
-                  onChange={(event) =>
-                    setCalories(parseFloat(event.target.value))
-                  }
-                  variant="outlined"
-                  fullWidth
-                  required
-                />
-              </Grid>
-              <Grid item xs={12} sm={6}>
-                <LocalizationProvider dateAdapter={AdapterDateFns}>
-                  <DateTimePicker
-                    renderInput={(props) => (
-                      <TextField {...props} fullWidth required />
-                    )}
-                    label="Consumed At"
-                    value={consumedAt}
-                    onChange={(value) => setConsumedAt(value)}
-                  />
-                </LocalizationProvider>
-              </Grid>
+    <Box sx={{ textAlign: "center", mb: 4 }}>
+      <Button variant="outlined" onClick={handleClickOpen}>
+        Add new food entry
+      </Button>
+      <Dialog open={open} onClose={handleClose} fullWidth maxWidth="md">
+        <DialogTitle>Add new food entry</DialogTitle>
+        <DialogContent>
+          <DialogContentText mb={1}>
+            To create a new food entry, make sure to provide details of Product
+            Name, Cost, Calories and Date/Time Consumed At, as indicated below.{" "}
+          </DialogContentText>
+          <Grid container spacing={2}>
+            <Grid item xs={12} sm={8}>
+              <TextField
+                label="Product Name"
+                margin="none"
+                variant="standard"
+                value={productName}
+                onChange={(event) => setProductName(event.target.value)}
+                fullWidth
+                required
+              />
             </Grid>
-
-            <Box
-              sx={{
-                display: "flex",
-                justifyContent: "flex-end",
-              }}
-            >
-              <Button
-                variant="outlined"
-                sx={{ mr: 1 }}
-                size="large"
-                startIcon={<ClearIcon />}
-                onClick={() => handleClear()}
-              >
-                Clear
-              </Button>
-              <Button
-                variant="contained"
-                size="large"
-                endIcon={<SendIcon />}
-                onClick={() => handleSave()}
-              >
-                Save
-              </Button>
-            </Box>
-          </Paper>
-        </Box>
-      )}
+            <Grid item xs={12} sm={4}>
+              <TextField
+                label="Cost"
+                type="Number"
+                margin="none"
+                variant="standard"
+                value={cost}
+                onChange={(event) =>
+                  setCost(parseFloat(event.target.value) || "")
+                }
+                fullWidth
+                required
+              />
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                label="Calories"
+                type="Number"
+                margin="none"
+                variant="standard"
+                value={calories}
+                onChange={(event) =>
+                  setCalories(parseFloat(event.target.value) || "")
+                }
+                fullWidth
+                required
+              />
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <LocalizationProvider dateAdapter={AdapterDateFns}>
+                <DateTimePicker
+                  renderInput={(props) => (
+                    <TextField
+                      {...props}
+                      variant="standard"
+                      margin="none"
+                      fullWidth
+                      required
+                    />
+                  )}
+                  label="Consumed At"
+                  value={consumedAt}
+                  onChange={(value) => setConsumedAt(value)}
+                />
+              </LocalizationProvider>
+            </Grid>
+          </Grid>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose}>Cancel</Button>
+          <Button onClick={handleSave}>Save</Button>
+        </DialogActions>
+      </Dialog>
     </Box>
   );
 };
