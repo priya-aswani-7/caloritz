8405d82da175be38237ce9a9b4a46860ced06978
Priya Aswani <priya.aswani28139@gmail.com>
2022-05-10 20:36:50 +0400

    Add a modal for user to confirm deleting an entry

--- src/components/FoodEntry.js
+++ src/components/FoodEntry.js
@@ -4,10 +4,25 @@ import TableRow from "@mui/material/TableRow";
 import MoreVertIcon from "@mui/icons-material/MoreVert";
 import { grey } from "@mui/material/colors";
 import { Button, Popover, Typography } from "@mui/material";
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogContentText from "@mui/material/DialogContentText";
+import DialogTitle from "@mui/material/DialogTitle";
 
 export const FoodEntry = ({ id, productName, cost, calories, consumedAt }) => {
   const [anchorEl, setAnchorEl] = useState(null);
   const [consumedAtTimeString, setConsumedAtTimeString] = useState(null);
+  const [deleteOpen, setDeleteOpen] = React.useState(false);
+
+  const handleDeleteOpen = () => {
+    setDeleteOpen(true);
+  };
+
+  const handleDeleteClose = () => {
+    setDeleteOpen(false);
+  };
+
   const open = Boolean(anchorEl);
   const popOverId = open ? "simple-popover" : undefined;
 
@@ -79,11 +94,35 @@ export const FoodEntry = ({ id, productName, cost, calories, consumedAt }) => {
           <Button
             variant={null}
             sx={{ textTransform: "none", display: "block", width: "100%" }}
+            onClick={handleDeleteOpen}
           >
             <Typography variant="body2" sx={{ p: 0.7 }}>
               Delete
             </Typography>
           </Button>
+          <Dialog
+            open={deleteOpen}
+            onClose={handleDeleteClose}
+            aria-labelledby="alert-dialog-title"
+            aria-describedby="alert-dialog-description"
+          >
+            <DialogTitle id="alert-dialog-title">
+              Are you sure you want to delete this entry?
+            </DialogTitle>
+            <DialogContent>
+              <DialogContentText id="alert-dialog-description">
+                Your entry for {productName} (${cost}) having {calories}{" "}
+                calories, consumed at {consumedAtTimeString} will be deleted.
+                This action cannot be undone.
+              </DialogContentText>
+            </DialogContent>
+            <DialogActions>
+              <Button onClick={handleDeleteClose}>Cancel</Button>
+              <Button onClick={handleDeleteClose} autoFocus>
+                Delete
+              </Button>
+            </DialogActions>
+          </Dialog>
         </Popover>
       </TableCell>
     </TableRow>
