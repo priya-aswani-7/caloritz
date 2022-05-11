b4b37d53eec9e8e931aa92d85fa527f9be87269b
Priya Aswani <priya.aswani28139@gmail.com>
2022-05-11 23:33:27 +0400

    Store and export dummy constant data in foodEntriesData

---
+++ src/constants/foodEntriesData.js
@@ -0,0 +1,105 @@
+export const foodEntriesData = [
+  {
+    monthYear: new Date(2021, 11).getTime(),
+    foodEntries: [
+      {
+        productName: "Frozen yoghurt",
+        cost: 20.12,
+        calories: 159,
+        consumedAt: new Date("10:02 pm, 22 December 2021").getTime(),
+      },
+      {
+        productName: "Ice cream sandwich",
+        cost: 34.09,
+        calories: 237,
+        consumedAt: new Date("08:02 am, 23 December 2021").getTime(),
+      },
+    ],
+  },
+  {
+    monthYear: new Date(2022, 0).getTime(),
+    foodEntries: [
+      {
+        productName: "Ice cream sandwich",
+        cost: 34.09,
+        calories: 237,
+        consumedAt: new Date("08:02 am, 11 January 2022").getTime(),
+      },
+      {
+        productName: "Eclair",
+        cost: 45.32,
+        calories: 262,
+        consumedAt: new Date("05:00 pm, 12 January 2022").getTime(),
+      },
+      {
+        productName: "Cupcake",
+        cost: 22.34,
+        calories: 305,
+        consumedAt: new Date("11:03 pm, 23 January 2022").getTime(),
+      },
+    ],
+  },
+  {
+    monthYear: new Date(2022, 1).getTime(),
+    foodEntries: [
+      {
+        productName: "Frozen yoghurt",
+        cost: 20.12,
+        calories: 159,
+        consumedAt: new Date("10:02 pm, 5 February 2022").getTime(),
+      },
+    ],
+  },
+  {
+    monthYear: new Date(2022, 2).getTime(),
+    foodEntries: [
+      {
+        productName: "Frozen yoghurt",
+        cost: 20.12,
+        calories: 159,
+        consumedAt: new Date("10:02 pm, 11 March 2022").getTime(),
+      },
+      {
+        productName: "Ice cream sandwich",
+        cost: 34.09,
+        calories: 237,
+        consumedAt: new Date("08:02 am, 12 March 2022").getTime(),
+      },
+      {
+        productName: "Eclair",
+        cost: 45.32,
+        calories: 262,
+        consumedAt: new Date("05:00 pm, 13 March 2022").getTime(),
+      },
+      {
+        productName: "Cupcake",
+        cost: 22.34,
+        calories: 305,
+        consumedAt: new Date("11:03 pm, 13 March 2022").getTime(),
+      },
+      {
+        productName: "Gingerbread",
+        cost: 11.34,
+        calories: 356,
+        consumedAt: new Date("09:27 pm, 13 March 2022").getTime(),
+      },
+    ],
+  },
+  {
+    monthYear: new Date(2022, 4).getTime(),
+    foodEntries: [
+      {
+        productName: "Cupcake",
+        cost: 22.34,
+        calories: 305,
+        consumedAt: new Date("11:03 pm, 20 April 2022").getTime(),
+      },
+      {
+        productName: "Gingerbread",
+        cost: 11.34,
+        calories: 356,
+        consumedAt: new Date("09:27 pm, 29 April 2022").getTime(),
+      },
+    ],
+  },
+];
