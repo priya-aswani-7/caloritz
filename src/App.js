149fff20bf81b4c374a2450515f6da62c6416384
Priya Aswani <priya.aswani28139@gmail.com>
2022-05-12 00:24:08 +0400

    Create and set monthly budget to 1000

--- src/App.js
+++ src/App.js
@@ -4,12 +4,13 @@ import { foodEntriesData } from "./constants";
 
 export const App = () => {
   const [data, setData] = useState(foodEntriesData);
+  const [monthlyBudget, setMonthlyBudget] = useState(1000);
 
   return (
     <>
       <NavBar />
       <FoodEntryInput data={data} setData={setData} />
-      <FoodEntryList data={data} setData={data} />
+      <FoodEntryList data={data} monthlyBudget={monthlyBudget} />
     </>
   );
 };
