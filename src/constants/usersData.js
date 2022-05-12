1b3ed2989b0229e093aa85aa80ff5d5b5715afbb
Priya Aswani <priya.aswani28139@gmail.com>
2022-05-12 19:01:27 +0400

    Create usersData

---
+++ src/constants/usersData.js
@@ -0,0 +1,7 @@
+export const usersData = [
+  { id: 1, name: "Alicia Stephens" },
+  { id: 2, name: "Jen Stone" },
+  { id: 3, name: "John Mitkins" },
+  { id: 4, name: "Peter Parker" },
+  { id: 5, name: "Sue White" },
+];
