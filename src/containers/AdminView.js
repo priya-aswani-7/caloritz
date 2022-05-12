8c60002c49d20ff2e1fc9d1c5c19844442e2ed53
Priya Aswani <priya.aswani28139@gmail.com>
2022-05-12 19:04:12 +0400

    Refactor props to FoodEntryInput in AdminView

--- src/containers/AdminView.js
+++ src/containers/AdminView.js
@@ -1,14 +1,20 @@
 import { useState } from "react";
 import { FoodEntryInput, GlobalFoodEntryList, NavBar } from "../components";
-import { globalFoodEntriesData } from "../constants";
+import { globalFoodEntriesData, usersData } from "../constants";
 
 export const AdminView = () => {
   const [data, setData] = useState(globalFoodEntriesData);
+  const [users, setUsers] = useState(usersData);
 
   return (
     <>
       <NavBar />
-      <FoodEntryInput />
+      <FoodEntryInput
+        data={data}
+        setData={setData}
+        isAdmin={true}
+        users={users}
+      />
       <GlobalFoodEntryList data={data} />
     </>
   );
