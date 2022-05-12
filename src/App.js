b0321a0624511998ee9bdce24b7991f765c92cf6
Priya Aswani <priya.aswani28139@gmail.com>
2022-05-12 12:04:06 +0400

    Declare and set dailyCalorieLimit in App

--- src/App.js
+++ src/App.js
@@ -5,12 +5,17 @@ import { foodEntriesData } from "./constants";
 export const App = () => {
   const [data, setData] = useState(foodEntriesData);
   const [monthlyBudget, setMonthlyBudget] = useState(1000);
+  const [dailyCalorieLimit, setDailyCalorieLimit] = useState(2100);
 
   return (
     <>
       <NavBar />
       <FoodEntryInput data={data} setData={setData} />
-      <FoodEntryList data={data} monthlyBudget={monthlyBudget} />
+      <FoodEntryList
+        data={data}
+        monthlyBudget={monthlyBudget}
+        dailyCalorieLimit={dailyCalorieLimit}
+      />
     </>
   );
 };
