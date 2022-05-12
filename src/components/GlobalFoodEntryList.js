034195b64264357036f7fba7802b2663c45efc34
Priya Aswani <priya.aswani28139@gmail.com>
2022-05-12 20:28:06 +0400

    Create a delete handler in GlobalFoodEntryList

--- src/components/GlobalFoodEntryList.js
+++ src/components/GlobalFoodEntryList.js
@@ -5,9 +5,16 @@ import TableCell from "@mui/material/TableCell";
 import TableContainer from "@mui/material/TableContainer";
 import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
+import { cloneDeep } from "lodash";
 import { GlobalFoodEntry } from "./";
 
-export const GlobalFoodEntryList = ({ data }) => {
+export const GlobalFoodEntryList = ({ data, setData }) => {
+  const handleDelete = (deleteIndex) => {
+    let currentData = data ? cloneDeep(data) : [];
+    currentData.splice(deleteIndex, 1);
+    setData(currentData);
+  };
+
   return (
     <TableContainer>
       <Box sx={{ maxWidth: 900, px: 5, mx: "auto" }}>
@@ -26,9 +33,10 @@ export const GlobalFoodEntryList = ({ data }) => {
           <TableBody>
             {data.map((foodEntry, index) => (
               <GlobalFoodEntry
+                key={index.toString()}
                 index={index}
                 {...foodEntry}
-                key={index.toString()}
+                handleDelete={handleDelete}
               />
             ))}
           </TableBody>
