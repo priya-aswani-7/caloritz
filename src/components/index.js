12cdf03fec75477c54959f6a59f92d5f6ac45c28
Priya Aswani <priya.aswani28139@gmail.com>
2022-05-12 20:13:08 +0400

    Export GlobalFoodEntry via components' index.js

--- src/components/index.js
+++ src/components/index.js
@@ -3,4 +3,5 @@ export { Filter } from "./Filter";
 export { FoodEntry } from "./FoodEntry";
 export { FoodEntryInput } from "./FoodEntryInput";
 export { FoodEntryList } from "./FoodEntryList";
+export { GlobalFoodEntry } from "./GlobalFoodEntry";
 export { GlobalFoodEntryList } from "./GlobalFoodEntryList";
