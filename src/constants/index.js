84749f6d8a81eeb9724132632c72f83cb90db697
Priya Aswani <priya.aswani28139@gmail.com>
2022-05-13 15:09:10 +0400

    Export statisticsData via constants' index.js

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,3 +1,4 @@
 export { foodEntriesData } from "./foodEntriesData";
 export { globalFoodEntriesData } from "./globalFoodEntriesData";
+export { statisticsData } from "./statisticsData";
 export { usersData } from "./usersData";
