afc367a1f619d631e88089802f1db02ed848b79e
Priya Aswani <priya.aswani28139@gmail.com>
2022-05-13 16:19:37 +0400

    Add content header to StatisticCardSet

--- src/components/StatisticCardSet.js
+++ src/components/StatisticCardSet.js
@@ -8,8 +8,11 @@ import { statisticsData } from "../constants";
 export const StatisticCardSet = () => {
   const nowString = getDateMonthYearString(new Date().getTime());
   return (
-    <Box sx={{ mx: 5, textAlign: "center" }}>
-      <Typography variant="h5" sx={{ fontWeight: 100, mb: 2 }}>
+    <Box sx={{ mx: 5, textAlign: "center", mb: 5 }}>
+      <Typography variant="overline" sx={{ fontWeight: 100, fontSize: 18 }}>
+        Food entry counts of the last week vs the last fortnight
+      </Typography>
+      <Typography variant="body1" sx={{ fontWeight: 100, mb: 2 }}>
         Today: {nowString}
       </Typography>
       <Box
