a1c51ee2e1001e2edb0a98adb5df527ec0f219dc
Priya Aswani <priya.aswani28139@gmail.com>
2022-05-13 18:35:27 +0400

    Style data in StatisticCalorieEntry

--- src/components/StatisticCalorieEntry.js
+++ src/components/StatisticCalorieEntry.js
@@ -4,9 +4,15 @@ import TableRow from "@mui/material/TableRow";
 export const StatisticCalorieEntry = ({ index, userName, averageCalories }) => {
   return (
     <TableRow key={index.toString()}>
-      <TableCell align="center">{index + 1}</TableCell>
-      <TableCell align="center">{userName}</TableCell>
-      <TableCell align="center">{averageCalories.toFixed(2)}</TableCell>
+      <TableCell sx={{ color: "text.secondary" }} align="center">
+        {index + 1}
+      </TableCell>
+      <TableCell sx={{ color: "text.secondary" }} align="center">
+        {userName}
+      </TableCell>
+      <TableCell sx={{ color: "text.secondary" }} align="center">
+        {averageCalories.toFixed(2)}
+      </TableCell>
     </TableRow>
   );
 };
