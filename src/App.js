16f7f5e4f9cfb848e349458f87bcc12da7cd99b1
Priya Aswani <priya.aswani28139@gmail.com>
2022-05-13 13:37:57 +0400

    Wrap App in BrowserRouter

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import { useState } from "react";
 import { AdminView, UserView } from "./containers";
+import { BrowserRouter as Router } from "react-router-dom";
 
 export const App = () => {
   const [isAdmin, setIsAdmin] = useState(true);
 
-  return isAdmin ? <AdminView /> : <UserView />;
+  return <Router> {isAdmin ? <AdminView /> : <UserView />}</Router>;
 };
