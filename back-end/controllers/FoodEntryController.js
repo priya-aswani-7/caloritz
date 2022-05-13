24f848338d691014dc2effb11aa3c313636ce7c2
Priya Aswani <priya.aswani28139@gmail.com>
2022-05-14 01:28:20 +0400

    Post process data in get endpoint for food entry

--- back-end/controllers/FoodEntryController.js
+++ back-end/controllers/FoodEntryController.js
@@ -6,7 +6,18 @@ module.exports = {
       FoodEntry.find(params)
         .populate("user", { _id: 1, name: 1 })
         .sort("consumedAt")
-        .then((data) => resolve(data))
+        .lean()
+        .then((data) => {
+          data = data.map((datum) => {
+            datum.userId = datum.user._id;
+            datum.userName = datum.user.name;
+            delete datum.user._id;
+            delete datum.user.name;
+            return datum;
+          });
+
+          resolve(data);
+        })
         .catch((error) => reject(error));
     });
   },
