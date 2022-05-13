3ce586803f91c36934f73818614e5a69a02ca980
Priya Aswani <priya.aswani28139@gmail.com>
2022-05-13 17:32:47 +0400

    Style the app using the custom theme

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import { useState } from "react";
+import { ThemeProvider } from "@mui/material/styles";
 import { AdminView, UserView } from "./containers";
 import { BrowserRouter as Router } from "react-router-dom";
+import { theme } from "./utils/theme";
 
 export const App = () => {
   const [isAdmin, setIsAdmin] = useState(true);
 
-  return <Router> {isAdmin ? <AdminView /> : <UserView />}</Router>;
+  return (
+    <Router>
+      <ThemeProvider theme={theme}>
+        {isAdmin ? <AdminView /> : <UserView />}
+      </ThemeProvider>
+    </Router>
+  );
 };
