0fcaf9f224ac55f03f7704debdfdd97f8d797f86
Priya Aswani <priya.aswani28139@gmail.com>
2022-05-13 15:24:11 +0400

    Render StatisticCardSet in Statistics

--- src/components/Statistics.js
+++ src/components/Statistics.js
@@ -1,27 +1,6 @@
 import * as React from "react";
-import Box from "@mui/material/Box";
-import Typography from "@mui/material/Typography";
-import { StatisticCard } from "./";
-import { getDateMonthYearString } from "../utils/helpers";
-import { statisticsData } from "../constants";
+import { StatisticCardSet } from "./";
 
 export const Statistics = () => {
-  const nowString = getDateMonthYearString(new Date().getTime());
-  return (
-    <Box sx={{ mx: 5, textAlign: "center" }}>
-      <Typography variant="h5" sx={{ fontWeight: 100, mb: 2 }}>
-        Today: {nowString}
-      </Typography>
-      <Box
-        sx={{
-          display: "flex",
-          justifyContent: "center",
-        }}
-      >
-        {statisticsData?.map((statistic, index) => (
-          <StatisticCard key={index.toString()} {...statistic} />
-        ))}
-      </Box>
-    </Box>
-  );
+  return <StatisticCardSet />;
 };
