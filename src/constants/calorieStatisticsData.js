2a15dcbed854fd2d3ae009feb62086f9bd1a3250
Priya Aswani <priya.aswani28139@gmail.com>
2022-05-13 16:16:50 +0400

    Create model calorieStatisticsData

---
+++ src/constants/calorieStatisticsData.js
@@ -0,0 +1,7 @@
+export const calorieStatisticsData = [
+  { userId: 1, userName: "Alicia Stephens", averageCalories: 2034.2 },
+  { userId: 2, userName: "Jen Stone", averageCalories: 1263.12 },
+  { userId: 3, userName: "John Mitkins", averageCalories: 2500.08 },
+  { userId: 4, userName: "Peter Parker", averageCalories: 2145.67 },
+  { userId: 5, userName: "Sue White", averageCalories: 1807.23 },
+];
