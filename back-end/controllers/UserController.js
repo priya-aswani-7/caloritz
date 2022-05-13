08117d6eb3f40903aa073a61199dc3d9075bfce4
Priya Aswani <priya.aswani28139@gmail.com>
2022-05-13 23:32:05 +0400

    Update UserController get endpoint to sort users by name

--- back-end/controllers/UserController.js
+++ back-end/controllers/UserController.js
@@ -4,6 +4,7 @@ module.exports = {
   get: (params) => {
     return new Promise((resolve, reject) => {
       User.find(params)
+        .sort("name")
         .then((data) => resolve(data))
         .catch((error) => reject(error));
     });
