a33e1683f1b45e5915c8b81257cf54fea0ed37a9
Priya Aswani <priya.aswani28139@gmail.com>
2022-05-14 00:08:21 +0400

    Add getById endpoint to UserController

--- back-end/controllers/UserController.js
+++ back-end/controllers/UserController.js
@@ -9,4 +9,18 @@ module.exports = {
         .catch((error) => reject(error));
     });
   },
+
+  getById: (id) => {
+    return new Promise((resolve, reject) => {
+      User.findById(id)
+        .then((data) => resolve(data))
+        .catch((error) =>
+          reject(
+            error.message.includes("Cast to ObjectId failed")
+              ? new Error(`Could not find user with ID ${id}`)
+              : error
+          )
+        );
+    });
+  },
 };
