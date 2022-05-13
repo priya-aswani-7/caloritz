69835933a99902dc0ef399066d5ac7e25b2069db
Priya Aswani <priya.aswani28139@gmail.com>
2022-05-13 15:23:14 +0400

    Create StatisticCardSet component

---
+++ src/components/StatisticCardSet.js
@@ -0,0 +1,27 @@
+import * as React from "react";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import { StatisticCard } from ".";
+import { getDateMonthYearString } from "../utils/helpers";
+import { statisticsData } from "../constants";
+
+export const StatisticCardSet = () => {
+  const nowString = getDateMonthYearString(new Date().getTime());
+  return (
+    <Box sx={{ mx: 5, textAlign: "center" }}>
+      <Typography variant="h5" sx={{ fontWeight: 100, mb: 2 }}>
+        Today: {nowString}
+      </Typography>
+      <Box
+        sx={{
+          display: "flex",
+          justifyContent: "center",
+        }}
+      >
+        {statisticsData?.map((statistic, index) => (
+          <StatisticCard key={index.toString()} {...statistic} />
+        ))}
+      </Box>
+    </Box>
+  );
+};
