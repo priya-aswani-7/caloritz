3aee63ba5c2cc53e684f21973631831b8bc7505d
Priya Aswani <priya.aswani28139@gmail.com>
2022-05-13 15:08:45 +0400

    Export StatisticCard via components' index.js

--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,8 @@
-export { NavBar } from "./NavBar";
 export { Filter } from "./Filter";
 export { FoodEntry } from "./FoodEntry";
 export { FoodEntryInput } from "./FoodEntryInput";
 export { FoodEntryList } from "./FoodEntryList";
 export { GlobalFoodEntry } from "./GlobalFoodEntry";
 export { GlobalFoodEntryList } from "./GlobalFoodEntryList";
+export { NavBar } from "./NavBar";
+export { StatisticCard } from "./StatisticCard";
