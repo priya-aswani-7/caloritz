c3d9fae2fded9411824f9857b4b3869c89195536
Priya Aswani <priya.aswani28139@gmail.com>
2022-05-14 23:26:49 +0400

    Receive and use setLoading as props in FoodEntryInput

--- front-end/src/components/FoodEntryInput.js
+++ front-end/src/components/FoodEntryInput.js
@@ -16,7 +16,6 @@ import { DateTimePicker, LocalizationProvider } from "@mui/x-date-pickers";
 import { cloneDeep } from "lodash";
 import { getInsertPosition } from "../utils/helpers";
 import { editFoodEntry } from "../services/api";
-import { LoadingSpinner } from "../components";
 
 export const FoodEntryInput = ({
   data,
@@ -28,6 +27,7 @@ export const FoodEntryInput = ({
   handleClose,
   editModeIndex,
   setError,
+  setLoading,
 }) => {
   const [selectedUserIndex, setSelectedUserIndex] = useState(null);
   const [productName, setProductName] = useState(null);
@@ -35,7 +35,6 @@ export const FoodEntryInput = ({
   const [calories, setCalories] = useState(null);
   const [consumedAt, setConsumedAt] = useState(null);
   const [datePickerOpened, setDatePickerOpened] = useState(null);
-  const [loading, setLoading] = useState(null);
 
   useEffect(() => {
     if (editModeIndex !== null) {
@@ -182,182 +181,174 @@ export const FoodEntryInput = ({
   };
 
   return (
-    <>
-      {loading && <LoadingSpinner />}
-
-      <Box sx={{ textAlign: "center", mb: 4 }}>
-        <Button variant="outlined" onClick={handleClickOpen}>
-          Add new food entry
-        </Button>
-        <Dialog open={open} onClose={handleClose} fullWidth maxWidth="md">
-          <DialogTitle>
-            {editModeIndex !== null ? "Edit food entry" : "Add new food entry"}
-          </DialogTitle>
-          <DialogContent>
-            <DialogContentText mb={2}>
-              To {editModeIndex !== null ? "edit the" : "create a new"} food
-              entry, make sure to{" "}
-              {editModeIndex !== null ? "make desired changes to" : "provide"}{" "}
-              details of Product Name, Cost, Calories and Date/Time Consumed At,
-              as indicated below.{" "}
-            </DialogContentText>
-            <Grid container spacing={2}>
-              {isAdmin && (
-                <Grid item xs={12} sm={12}>
-                  <FormControl variant="standard" fullWidth required>
-                    <InputLabel id="demo-simple-select-standard-label">
-                      User
-                    </InputLabel>
-                    <Select
-                      labelId="demo-simple-select-standard-label"
-                      id="demo-simple-select-standard"
-                      value={selectedUserIndex ?? ""}
-                      onChange={(event) =>
-                        setSelectedUserIndex(event.target.value)
-                      }
-                      label="User"
-                    >
-                      {users?.map((user, index) => (
-                        <MenuItem key={index.toString()} value={index}>
-                          {user.name}
-                        </MenuItem>
-                      ))}
-                    </Select>
-                  </FormControl>
-                </Grid>
-              )}
-              <Grid item xs={12} sm={8}>
-                <TextField
-                  label="Product Name"
-                  margin="none"
-                  variant="standard"
-                  value={productName ?? ""}
-                  onChange={(event) => setProductName(event.target.value)}
-                  fullWidth
-                  required
-                  error={productName?.trim() === ""}
-                  helperText={
-                    productName?.trim() === ""
-                      ? "Product name is required"
-                      : null
-                  }
-                />
+    <Box sx={{ textAlign: "center", mb: 4 }}>
+      <Button variant="outlined" onClick={handleClickOpen}>
+        Add new food entry
+      </Button>
+      <Dialog open={open} onClose={handleClose} fullWidth maxWidth="md">
+        <DialogTitle>
+          {editModeIndex !== null ? "Edit food entry" : "Add new food entry"}
+        </DialogTitle>
+        <DialogContent>
+          <DialogContentText mb={2}>
+            To {editModeIndex !== null ? "edit the" : "create a new"} food
+            entry, make sure to{" "}
+            {editModeIndex !== null ? "make desired changes to" : "provide"}{" "}
+            details of Product Name, Cost, Calories and Date/Time Consumed At,
+            as indicated below.{" "}
+          </DialogContentText>
+          <Grid container spacing={2}>
+            {isAdmin && (
+              <Grid item xs={12} sm={12}>
+                <FormControl variant="standard" fullWidth required>
+                  <InputLabel id="demo-simple-select-standard-label">
+                    User
+                  </InputLabel>
+                  <Select
+                    labelId="demo-simple-select-standard-label"
+                    id="demo-simple-select-standard"
+                    value={selectedUserIndex ?? ""}
+                    onChange={(event) =>
+                      setSelectedUserIndex(event.target.value)
+                    }
+                    label="User"
+                  >
+                    {users?.map((user, index) => (
+                      <MenuItem key={index.toString()} value={index}>
+                        {user.name}
+                      </MenuItem>
+                    ))}
+                  </Select>
+                </FormControl>
               </Grid>
-              <Grid item xs={12} sm={4}>
-                <TextField
-                  label="Cost"
-                  type="Number"
-                  margin="none"
-                  variant="standard"
-                  value={cost ?? ""}
-                  onChange={(event) =>
-                    setCost(parseFloat(event.target.value) || "")
-                  }
-                  fullWidth
-                  required
-                  error={cost === "" || (cost !== null && cost <= 0)}
-                  helperText={
-                    cost === "" || cost === 0
-                      ? "Product cost is required"
-                      : cost < 0
-                      ? "Product cost must be positive"
-                      : null
-                  }
-                />
-              </Grid>
-              <Grid item xs={12} sm={6}>
-                <TextField
-                  label="Calories"
-                  type="Number"
-                  margin="none"
-                  variant="standard"
-                  value={calories ?? ""}
-                  onChange={(event) =>
-                    setCalories(parseFloat(event.target.value) || "")
-                  }
-                  fullWidth
-                  required
-                  error={
-                    calories === "" || (calories !== null && calories <= 0)
-                  }
-                  helperText={
-                    calories === "" || calories === 0
-                      ? "Product calories are required"
-                      : calories < 0
-                      ? "Product calories must be positive"
-                      : null
-                  }
-                />
-              </Grid>
-              <Grid item xs={12} sm={6}>
-                <LocalizationProvider dateAdapter={AdapterDateFns}>
-                  <DateTimePicker
-                    renderInput={(props) => (
-                      <TextField
-                        {...props}
-                        variant="standard"
-                        margin="none"
-                        fullWidth
-                        required
-                        error={
-                          (datePickerOpened &&
-                            (consumedAt === null || consumedAt === "")) ||
-                          consumedAt > new Date()
-                        }
-                        helperText={
-                          datePickerOpened &&
-                          (consumedAt === null || consumedAt === "")
-                            ? "Product consumption timestamp is required"
-                            : consumedAt > new Date()
-                            ? "Product consumption timestamp cannot be in the future"
-                            : null
-                        }
-                      />
-                    )}
-                    label="Consumed At"
-                    value={consumedAt}
-                    onChange={(value) => setConsumedAt(value)}
-                    onClose={() => !consumedAt && setDatePickerOpened(true)}
-                    maxDateTime={new Date()}
-                  />
-                </LocalizationProvider>
-              </Grid>
+            )}
+            <Grid item xs={12} sm={8}>
+              <TextField
+                label="Product Name"
+                margin="none"
+                variant="standard"
+                value={productName ?? ""}
+                onChange={(event) => setProductName(event.target.value)}
+                fullWidth
+                required
+                error={productName?.trim() === ""}
+                helperText={
+                  productName?.trim() === "" ? "Product name is required" : null
+                }
+              />
             </Grid>
-          </DialogContent>
-          <DialogActions sx={{ mr: 2, mb: 1 }}>
-            <Button onClick={handleClose} variant="outlined">
-              Cancel
-            </Button>
-            <Button
-              variant="contained"
-              onClick={
-                isAdmin
-                  ? editModeIndex !== null
-                    ? handleEdit
-                    : handleSaveAdminEntry
-                  : handleSaveUserEntry
-              }
-              disabled={
-                (isAdmin && selectedUserIndex === null) ||
-                !productName ||
-                !cost ||
-                !calories ||
-                !consumedAt ||
-                productName?.trim() === "" ||
-                cost === "" ||
-                cost <= 0 ||
-                calories === "" ||
-                calories <= 0 ||
-                (datePickerOpened &&
-                  (consumedAt === null || consumedAt === "")) ||
-                consumedAt > new Date()
-              }
-            >
-              {editModeIndex !== null ? "Save Changes" : "Create"}
-            </Button>
-          </DialogActions>
-        </Dialog>
-      </Box>
-    </>
+            <Grid item xs={12} sm={4}>
+              <TextField
+                label="Cost"
+                type="Number"
+                margin="none"
+                variant="standard"
+                value={cost ?? ""}
+                onChange={(event) =>
+                  setCost(parseFloat(event.target.value) || "")
+                }
+                fullWidth
+                required
+                error={cost === "" || (cost !== null && cost <= 0)}
+                helperText={
+                  cost === "" || cost === 0
+                    ? "Product cost is required"
+                    : cost < 0
+                    ? "Product cost must be positive"
+                    : null
+                }
+              />
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                label="Calories"
+                type="Number"
+                margin="none"
+                variant="standard"
+                value={calories ?? ""}
+                onChange={(event) =>
+                  setCalories(parseFloat(event.target.value) || "")
+                }
+                fullWidth
+                required
+                error={calories === "" || (calories !== null && calories <= 0)}
+                helperText={
+                  calories === "" || calories === 0
+                    ? "Product calories are required"
+                    : calories < 0
+                    ? "Product calories must be positive"
+                    : null
+                }
+              />
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <LocalizationProvider dateAdapter={AdapterDateFns}>
+                <DateTimePicker
+                  renderInput={(props) => (
+                    <TextField
+                      {...props}
+                      variant="standard"
+                      margin="none"
+                      fullWidth
+                      required
+                      error={
+                        (datePickerOpened &&
+                          (consumedAt === null || consumedAt === "")) ||
+                        consumedAt > new Date()
+                      }
+                      helperText={
+                        datePickerOpened &&
+                        (consumedAt === null || consumedAt === "")
+                          ? "Product consumption timestamp is required"
+                          : consumedAt > new Date()
+                          ? "Product consumption timestamp cannot be in the future"
+                          : null
+                      }
+                    />
+                  )}
+                  label="Consumed At"
+                  value={consumedAt}
+                  onChange={(value) => setConsumedAt(value)}
+                  onClose={() => !consumedAt && setDatePickerOpened(true)}
+                  maxDateTime={new Date()}
+                />
+              </LocalizationProvider>
+            </Grid>
+          </Grid>
+        </DialogContent>
+        <DialogActions sx={{ mr: 2, mb: 1 }}>
+          <Button onClick={handleClose} variant="outlined">
+            Cancel
+          </Button>
+          <Button
+            variant="contained"
+            onClick={
+              isAdmin
+                ? editModeIndex !== null
+                  ? handleEdit
+                  : handleSaveAdminEntry
+                : handleSaveUserEntry
+            }
+            disabled={
+              (isAdmin && selectedUserIndex === null) ||
+              !productName ||
+              !cost ||
+              !calories ||
+              !consumedAt ||
+              productName?.trim() === "" ||
+              cost === "" ||
+              cost <= 0 ||
+              calories === "" ||
+              calories <= 0 ||
+              (datePickerOpened &&
+                (consumedAt === null || consumedAt === "")) ||
+              consumedAt > new Date()
+            }
+          >
+            {editModeIndex !== null ? "Save Changes" : "Create"}
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </Box>
   );
 };
