88fff4d4c0acdffe048554d78e08afdb25f710b5
Priya Aswani <priya.aswani28139@gmail.com>
2022-05-14 19:18:37 +0400

    Add validation to FoodEntry model

--- back-end/models/FoodEntry.js
+++ back-end/models/FoodEntry.js
@@ -4,11 +4,28 @@ const FoodEntry = mongoose.Schema(
     user: {
       type: mongoose.Schema.Types.ObjectId,
       ref: "User",
+      required: "User ID is required.",
     },
-    productName: { type: String },
-    cost: { type: Number },
-    calories: { type: Number },
-    consumedAt: { type: Number },
+    productName: { type: String, required: "Product name is required." },
+    cost: {
+      type: Number,
+      required: "Product cost is required.",
+      min: [1, "Product cost must be positive."],
+    },
+    calories: {
+      type: Number,
+      required: "Product calories are required.",
+      min: [1, "Product calories must be positive."],
+    },
+    consumedAt: {
+      type: Number,
+      required: "Product consumption date is required.",
+      min: [0, "Product consumption date cannot precede Jan 1, 1970."],
+      max: [
+        new Date().getTime(),
+        "Product consumption date cannot be in the future.",
+      ],
+    },
   },
   { timestamps: true }
 );
