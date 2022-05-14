b0f7311665d2ba3a06ff4904c0994d91c7b355a6
Priya Aswani <priya.aswani28139@gmail.com>
2022-05-15 00:23:50 +0400

    Factor out state to live in Statistics

--- front-end/src/components/Statistics.js
+++ front-end/src/components/Statistics.js
@@ -1,12 +1,19 @@
-import * as React from "react";
+import { useState } from "react";
+import { calorieStatisticsData, foodEntryStatisticsData } from "../constants";
 import { StatisticCardSet } from "./";
 import { StatisticCalorieList } from "./StatisticCalorieList";
 
 export const Statistics = () => {
+  const [foodEntryStatistics, setFoodEntryStatistics] = useState(
+    foodEntryStatisticsData
+  );
+  const [calorieStatistics, setCalorieStatistics] = useState(
+    calorieStatisticsData
+  );
   return (
     <>
-      <StatisticCardSet />
-      <StatisticCalorieList />
+      <StatisticCardSet foodEntryStatistics={foodEntryStatistics} />
+      <StatisticCalorieList calorieStatistics={calorieStatistics} />
     </>
   );
 };
