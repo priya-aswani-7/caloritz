3e3cabadc95154296c85d9d802a36e2472fe6385
Priya Aswani <priya.aswani28139@gmail.com>
2022-05-15 01:07:18 +0400

    Use ErrorAlert, LoadingSpinner in Statistics

--- front-end/src/components/Statistics.js
+++ front-end/src/components/Statistics.js
@@ -2,6 +2,8 @@ import { useState, useEffect } from "react";
 import { calorieStatisticsData, foodEntryStatisticsData } from "../constants";
 import { getStatistics } from "../services/api";
 import { StatisticCardSet } from "./";
+import { ErrorAlert } from "./ErrorAlert";
+import { LoadingSpinner } from "./LoadingSpinner";
 import { StatisticCalorieList } from "./StatisticCalorieList";
 
 export const Statistics = () => {
@@ -15,16 +17,24 @@ export const Statistics = () => {
   const [error, setError] = useState(null);
 
   useEffect(() => {
-    getStatistics().then((data) => {
-      setError(null);
-      setCalorieStatistics(data.calorieStatisticsData);
-      setFoodEntryStatistics(data.foodEntryStatisticsData);
-      setLoading(false);
-    });
+    getStatistics()
+      .then((data) => {
+        setError(null);
+        setCalorieStatistics(data.calorieStatisticsData);
+        setFoodEntryStatistics(data.foodEntryStatisticsData);
+      })
+      .catch((error) => setError(error));
   }, []);
 
-  return (
+  useEffect(() => {
+    foodEntryStatistics && calorieStatistics && setLoading(false);
+  }, [foodEntryStatistics, calorieStatistics]);
+
+  return error ? (
+    <ErrorAlert />
+  ) : (
     <>
+      {loading && <LoadingSpinner />}
       <StatisticCardSet foodEntryStatistics={foodEntryStatistics} />
       <StatisticCalorieList calorieStatistics={calorieStatistics} />
     </>
