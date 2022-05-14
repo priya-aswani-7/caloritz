668eae738dfbcd4bf21cab88b4d37e1c5a95240d
Priya Aswani <priya.aswani28139@gmail.com>
2022-05-14 15:54:43 +0400

    Export StatisticController via controllers' index.js

--- back-end/controllers/index.js
+++ back-end/controllers/index.js
@@ -1,7 +1,9 @@
 const FoodEntryController = require("./FoodEntryController");
+const StatisticController = require("./StatisticController");
 const UserController = require("./UserController");
 
 module.exports = {
   foodentry: FoodEntryController,
+  statistic: StatisticController,
   user: UserController,
 };
