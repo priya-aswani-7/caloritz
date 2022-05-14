d8d50b6967b782bfb8e5edaeeec68c3e0a60a6ce
Priya Aswani <priya.aswani28139@gmail.com>
2022-05-14 20:16:44 +0400

    Display placeholder text in GlobalFoodEntryList if no data found

--- front-end/src/components/GlobalFoodEntryList.js
+++ front-end/src/components/GlobalFoodEntryList.js
@@ -1,3 +1,4 @@
+import { Typography } from "@mui/material";
 import Box from "@mui/material/Box";
 import Table from "@mui/material/Table";
 import TableBody from "@mui/material/TableBody";
@@ -15,36 +16,43 @@ export const GlobalFoodEntryList = ({ data, setData, handleEdit }) => {
     setData(currentData);
   };
 
-  return (
-    data?.length > 0 && (
-      <TableContainer>
-        <Box sx={{ maxWidth: 925, px: 5, mx: "auto" }}>
-          <Table size="small" aria-label="purchases" sx={{ mb: 2 }}>
-            <TableHead>
-              <TableRow>
-                <TableCell>Index</TableCell>
-                <TableCell>User Name</TableCell>
-                <TableCell>Product Name</TableCell>
-                <TableCell>Cost ($)</TableCell>
-                <TableCell>Calories</TableCell>
-                <TableCell>Consumed At</TableCell>
-                <TableCell />
-              </TableRow>
-            </TableHead>
-            <TableBody>
-              {data.map((foodEntry, index) => (
-                <GlobalFoodEntry
-                  key={index.toString()}
-                  index={index}
-                  {...foodEntry}
-                  handleDelete={handleDelete}
-                  handleEdit={handleEdit}
-                />
-              ))}
-            </TableBody>
-          </Table>
-        </Box>
-      </TableContainer>
+  return data?.length > 0 ? (
+    <TableContainer>
+      <Box sx={{ maxWidth: 925, px: 5, mx: "auto" }}>
+        <Table size="small" aria-label="purchases" sx={{ mb: 2 }}>
+          <TableHead>
+            <TableRow>
+              <TableCell>Index</TableCell>
+              <TableCell>User Name</TableCell>
+              <TableCell>Product Name</TableCell>
+              <TableCell>Cost ($)</TableCell>
+              <TableCell>Calories</TableCell>
+              <TableCell>Consumed At</TableCell>
+              <TableCell />
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {data.map((foodEntry, index) => (
+              <GlobalFoodEntry
+                key={index.toString()}
+                index={index}
+                {...foodEntry}
+                handleDelete={handleDelete}
+                handleEdit={handleEdit}
+              />
+            ))}
+          </TableBody>
+        </Table>
+      </Box>
+    </TableContainer>
+  ) : (
+    data?.length === 0 && (
+      <Typography
+        sx={{ textAlign: "center", fontWeight: 100 }}
+        color="text.secondary"
+      >
+        There are no current food entries. Tap above to create one :)
+      </Typography>
     )
   );
 };
