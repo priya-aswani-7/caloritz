768193fa3be48fcfaedbc81aa47871eaa07999aa
Priya Aswani <priya.aswani28139@gmail.com>
2022-05-14 22:37:49 +0400

    Add editFoodEntry to api.js

--- front-end/src/services/api.js
+++ front-end/src/services/api.js
@@ -27,3 +27,18 @@ export const getUsers = async () => {
     .then((data) => data.data)
     .catch((error) => error);
 };
+
+export const editFoodEntry = async (foodEntryId, updatedFoodEntry) => {
+  return fetch(`${BASE_URL}/foodentry/${foodEntryId}`, {
+    mode: "cors",
+    method: "put",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(updatedFoodEntry),
+  })
+    .then((response) => response.json())
+    .then((data) => data.data)
+    .catch((error) => error);
+};
