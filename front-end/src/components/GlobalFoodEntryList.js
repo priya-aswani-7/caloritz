2a1706209721a7d804b02d51dfb6ba90bcad6bc3
Priya Aswani <priya.aswani28139@gmail.com>
2022-05-14 23:03:07 +0400

    Use deleteFoodEntry in GlobalFoodEntryList

--- front-end/src/components/GlobalFoodEntryList.js
+++ front-end/src/components/GlobalFoodEntryList.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { Typography } from "@mui/material";
 import Box from "@mui/material/Box";
 import Table from "@mui/material/Table";
@@ -7,52 +8,69 @@ import TableContainer from "@mui/material/TableContainer";
 import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
 import { cloneDeep } from "lodash";
+import { deleteFoodEntry } from "../services/api";
 import { GlobalFoodEntry } from "./";
+import { LoadingSpinner } from "./LoadingSpinner";
 
 export const GlobalFoodEntryList = ({ data, setData, handleEdit }) => {
+  const [loading, setLoading] = useState(null);
+  const [error, setError] = useState(null);
+
   const handleDelete = (deleteIndex) => {
-    let currentData = data ? cloneDeep(data) : [];
-    currentData.splice(deleteIndex, 1);
-    setData(currentData);
+    setLoading(true);
+    deleteFoodEntry(data[deleteIndex]?._id)
+      .then(() => {
+        setError(null);
+        let currentData = data ? cloneDeep(data) : [];
+        currentData.splice(deleteIndex, 1);
+        setData(currentData);
+        setLoading(false);
+      })
+      .catch((error) => setError(error));
   };
 
-  return data?.length > 0 ? (
-    <TableContainer>
-      <Box sx={{ maxWidth: 925, px: 5, mx: "auto" }}>
-        <Table size="small" aria-label="purchases" sx={{ mb: 2 }}>
-          <TableHead>
-            <TableRow>
-              <TableCell>Index</TableCell>
-              <TableCell>User Name</TableCell>
-              <TableCell>Product Name</TableCell>
-              <TableCell>Cost ($)</TableCell>
-              <TableCell>Calories</TableCell>
-              <TableCell>Consumed At</TableCell>
-              <TableCell />
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {data.map((foodEntry, index) => (
-              <GlobalFoodEntry
-                key={index.toString()}
-                index={index}
-                {...foodEntry}
-                handleDelete={handleDelete}
-                handleEdit={handleEdit}
-              />
-            ))}
-          </TableBody>
-        </Table>
-      </Box>
-    </TableContainer>
-  ) : (
-    data?.length === 0 && (
-      <Typography
-        sx={{ textAlign: "center", fontWeight: 100 }}
-        color="text.secondary"
-      >
-        There are no current food entries. Tap above to create one :)
-      </Typography>
-    )
+  return (
+    <>
+      {loading && <LoadingSpinner />}
+      {data?.length > 0 ? (
+        <TableContainer>
+          <Box sx={{ maxWidth: 925, px: 5, mx: "auto" }}>
+            <Table size="small" aria-label="purchases" sx={{ mb: 2 }}>
+              <TableHead>
+                <TableRow>
+                  <TableCell>Index</TableCell>
+                  <TableCell>User Name</TableCell>
+                  <TableCell>Product Name</TableCell>
+                  <TableCell>Cost ($)</TableCell>
+                  <TableCell>Calories</TableCell>
+                  <TableCell>Consumed At</TableCell>
+                  <TableCell />
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {data.map((foodEntry, index) => (
+                  <GlobalFoodEntry
+                    key={index.toString()}
+                    index={index}
+                    {...foodEntry}
+                    handleDelete={handleDelete}
+                    handleEdit={handleEdit}
+                  />
+                ))}
+              </TableBody>
+            </Table>
+          </Box>
+        </TableContainer>
+      ) : (
+        data?.length === 0 && (
+          <Typography
+            sx={{ textAlign: "center", fontWeight: 100 }}
+            color="text.secondary"
+          >
+            There are no current food entries. Tap above to create one :)
+          </Typography>
+        )
+      )}
+    </>
   );
 };
