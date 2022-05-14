287672ccb4fc5d01edd42e3223faea2db6229d3f
Priya Aswani <priya.aswani28139@gmail.com>
2022-05-15 01:39:29 +0400

    Use getFoodEntriesByUserId in UserView

--- front-end/src/containers/UserView.js
+++ front-end/src/containers/UserView.js
@@ -1,36 +1,64 @@
-import { useState } from "react";
-import { NavBar, FoodEntryList, FoodEntryInput } from "../components";
-import { foodEntriesData } from "../constants";
+import { useState, useEffect } from "react";
+import {
+  NavBar,
+  FoodEntryList,
+  FoodEntryInput,
+  ErrorAlert,
+  LoadingSpinner,
+} from "../components";
+import { getFoodEntriesByUserId } from "../services/api";
 
 export const UserView = () => {
-  const [data, setData] = useState(foodEntriesData);
+  const [userId, setUserId] = useState("627eb18aaf86485f3310d00e");
+  const [data, setData] = useState(null);
   const [monthlyBudget, setMonthlyBudget] = useState(1000);
   const [dailyCalorieLimit, setDailyCalorieLimit] = useState(2100);
   const [filterStartDate, setFilterStartDate] = useState(null);
   const [filterEndDate, setFilterEndDate] = useState(null);
   const [open, setOpen] = useState(false);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    getFoodEntriesByUserId(userId)
+      .then((data) => {
+        setError(null);
+        setData(data);
+        setLoading(false);
+      })
+      .catch((error) => setError(error));
+  }, []);
 
   return (
     <>
       <NavBar />
-      <FoodEntryInput
-        data={data}
-        setData={setData}
-        isAdmin={false}
-        open={open}
-        handleClickOpen={() => setOpen(true)}
-        handleClose={() => setOpen(false)}
-        editModeIndex={null}
-      />
-      <FoodEntryList
-        data={data}
-        monthlyBudget={monthlyBudget}
-        dailyCalorieLimit={dailyCalorieLimit}
-        filterStartDate={filterStartDate}
-        filterEndDate={filterEndDate}
-        setFilterStartDate={setFilterStartDate}
-        setFilterEndDate={setFilterEndDate}
-      />
+      {error ? (
+        <ErrorAlert />
+      ) : (
+        <>
+          {loading && <LoadingSpinner />}
+          <FoodEntryInput
+            data={data}
+            setData={setData}
+            isAdmin={false}
+            open={open}
+            handleClickOpen={() => setOpen(true)}
+            handleClose={() => setOpen(false)}
+            editModeIndex={null}
+            setError={setError}
+            setLoading={setLoading}
+          />
+          <FoodEntryList
+            data={data}
+            monthlyBudget={monthlyBudget}
+            dailyCalorieLimit={dailyCalorieLimit}
+            filterStartDate={filterStartDate}
+            filterEndDate={filterEndDate}
+            setFilterStartDate={setFilterStartDate}
+            setFilterEndDate={setFilterEndDate}
+          />
+        </>
+      )}
     </>
   );
 };
