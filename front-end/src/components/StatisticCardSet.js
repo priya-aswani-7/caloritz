782f3cb748b722543a0c99e57df3789e1bed107d
Priya Aswani <priya.aswani28139@gmail.com>
2022-05-15 01:00:10 +0400

    Refine use of foodEntryStatistics in StatisticCardSet

--- front-end/src/components/StatisticCardSet.js
+++ front-end/src/components/StatisticCardSet.js
@@ -1,10 +1,15 @@
+import { useState, useEffect } from "react";
 import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
 import { StatisticCard } from ".";
 import { getDateMonthYearString } from "../utils/helpers";
 
 export const StatisticCardSet = ({ foodEntryStatistics }) => {
-  const nowString = getDateMonthYearString(new Date().getTime());
+  const [nowString, setNowString] = useState(null);
+
+  useEffect(() => {
+    setNowString(getDateMonthYearString(foodEntryStatistics?.now));
+  }, [foodEntryStatistics]);
 
   return (
     <Box sx={{ mx: 5, textAlign: "center", mb: 4 }}>
@@ -24,7 +29,7 @@ export const StatisticCardSet = ({ foodEntryStatistics }) => {
           justifyContent: "center",
         }}
       >
-        {foodEntryStatistics?.map((statistic, index) => (
+        {foodEntryStatistics?.foodEntryCounts?.map((statistic, index) => (
           <StatisticCard key={index.toString()} {...statistic} />
         ))}
       </Box>
