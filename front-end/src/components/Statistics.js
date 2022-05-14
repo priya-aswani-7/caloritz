408403529bb64dfcb4e4313a6c15da0cfb550cf0
Priya Aswani <priya.aswani28139@gmail.com>
2022-05-15 00:59:07 +0400

    Use getStatistics in Statistics

--- front-end/src/components/Statistics.js
+++ front-end/src/components/Statistics.js
@@ -1,5 +1,6 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { calorieStatisticsData, foodEntryStatisticsData } from "../constants";
+import { getStatistics } from "../services/api";
 import { StatisticCardSet } from "./";
 import { StatisticCalorieList } from "./StatisticCalorieList";
 
@@ -10,6 +11,18 @@ export const Statistics = () => {
   const [calorieStatistics, setCalorieStatistics] = useState(
     calorieStatisticsData
   );
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    getStatistics().then((data) => {
+      setError(null);
+      setCalorieStatistics(data.calorieStatisticsData);
+      setFoodEntryStatistics(data.foodEntryStatisticsData);
+      setLoading(false);
+    });
+  }, []);
+
   return (
     <>
       <StatisticCardSet foodEntryStatistics={foodEntryStatistics} />
