77044769573ff294dbe0226e6da372b5d7e320f3
Priya Aswani <priya.aswani28139@gmail.com>
2022-05-14 19:19:04 +0400

    Add validation to User model

--- back-end/models/User.js
+++ back-end/models/User.js
@@ -1,7 +1,11 @@
 const mongoose = require("mongoose");
 const User = mongoose.Schema({
-  name: { type: String },
-  type: { type: String, enum: ["user", "admin"] },
+  name: { type: String, required: "User name is required." },
+  type: {
+    type: String,
+    enum: ["user", "admin"],
+    required: "User type is required.",
+  },
 });
 
 module.exports = mongoose.model("User", User);
