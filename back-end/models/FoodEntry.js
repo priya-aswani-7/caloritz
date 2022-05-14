ef4e39db8bf5194a0557295093bd1d57e7b006aa
Priya Aswani <priya.aswani28139@gmail.com>
2022-05-14 15:17:45 +0400

    Timestamp food entry documents for creation and updation

--- back-end/models/FoodEntry.js
+++ back-end/models/FoodEntry.js
@@ -1,13 +1,16 @@
 const mongoose = require("mongoose");
-const FoodEntry = mongoose.Schema({
-  user: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "User",
+const FoodEntry = mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+    productName: { type: String },
+    cost: { type: Number },
+    calories: { type: Number },
+    consumedAt: { type: Number },
   },
-  productName: { type: String },
-  cost: { type: Number },
-  calories: { type: Number },
-  consumedAt: { type: Number },
-});
+  { timestamps: true }
+);
 
 module.exports = mongoose.model("FoodEntry", FoodEntry);
