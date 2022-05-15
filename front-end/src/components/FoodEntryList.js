3bb22edbd613cbcc0fbf933fa4b494ffba4c5b31
Priya Aswani <priya.aswani28139@gmail.com>
2022-05-15 15:02:38 +0400

    Add placeholder for when no results in selected date range

--- front-end/src/components/FoodEntryList.js
+++ front-end/src/components/FoodEntryList.js
@@ -17,36 +17,47 @@ export const FoodEntryList = ({
   setFilterStartDate,
   setFilterEndDate,
 }) => {
-  return data?.length > 0 ? (
-    <TableContainer>
-      <Box sx={{ maxWidth: 925, px: 5, mx: "auto" }}>
-        <Table aria-label="collapsible table" size="small">
-          <TableHead>
-            <TableRow>
-              <TableCell>Month, Year</TableCell>
-              <TableCell align="right">
-                <Filter
-                  filterStartDate={filterStartDate}
-                  filterEndDate={filterEndDate}
-                  setFilterStartDate={setFilterStartDate}
-                  setFilterEndDate={setFilterEndDate}
+  return data?.length > 0 || (filterStartDate && filterEndDate) ? (
+    <>
+      <TableContainer>
+        <Box sx={{ maxWidth: 925, px: 5, mx: "auto" }}>
+          <Table aria-label="collapsible table" size="small">
+            <TableHead>
+              <TableRow>
+                <TableCell>Month, Year</TableCell>
+                <TableCell align="right">
+                  <Filter
+                    filterStartDate={filterStartDate}
+                    filterEndDate={filterEndDate}
+                    setFilterStartDate={setFilterStartDate}
+                    setFilterEndDate={setFilterEndDate}
+                  />
+                </TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {data?.map((monthlyFoodEntry, index) => (
+                <FoodEntry
+                  key={index.toString()}
+                  monthlyFoodEntry={monthlyFoodEntry}
+                  monthlyBudget={monthlyBudget}
+                  dailyCalorieLimit={dailyCalorieLimit}
                 />
-              </TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {data?.map((monthlyFoodEntry, index) => (
-              <FoodEntry
-                key={index.toString()}
-                monthlyFoodEntry={monthlyFoodEntry}
-                monthlyBudget={monthlyBudget}
-                dailyCalorieLimit={dailyCalorieLimit}
-              />
-            ))}
-          </TableBody>
-        </Table>
-      </Box>
-    </TableContainer>
+              ))}
+            </TableBody>
+          </Table>
+        </Box>
+      </TableContainer>
+      {data?.length === 0 && (
+        <Typography
+          sx={{ textAlign: "center", fontWeight: 100, mt: 2 }}
+          color="text.secondary"
+        >
+          There don't seem to be any food entries in the selected range. Play
+          around with the range to see results :)
+        </Typography>
+      )}
+    </>
   ) : (
     (data === undefined || data?.length === 0) && (
       <Typography
