2040c6f131cc970355b48fea42c4611f85cfd2f6
Priya Aswani <priya.aswani28139@gmail.com>
2022-05-15 17:11:10 +0400

    Validate password before hashing in post endpoint of UserController

--- back-end/controllers/UserController.js
+++ back-end/controllers/UserController.js
@@ -28,12 +28,21 @@ module.exports = {
 
   post: async (params) => {
     try {
-      const password = await bcrypt.hash(params.password, 10);
       const oldUser = await User.findOne({ email: params.email });
       if (oldUser) {
         throw new Error(`User with email ${params.email} already exists.`);
       }
 
+      let { password } = params;
+      if (password.length === 0) {
+        throw new Error("User password is required.");
+      } else if (password.length < 8) {
+        throw new Error("User password must be minimum 8 characters.");
+      } else if (password.length > 128) {
+        throw new Error("User password must be at most 128 characters.");
+      }
+
+      password = await bcrypt.hash(password, 10);
       const user = await User.create({ ...params, password });
       const token = await jwt.sign(
         { userId: user._id, email: user.email, type: user.type },
@@ -42,6 +51,7 @@ module.exports = {
           expiresIn: "7d",
         }
       );
+
       const data = { ...user._doc, token };
       return data;
     } catch (error) {
