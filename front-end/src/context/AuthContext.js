129243dbd1268de61460f086dacedb9fda330900
Priya Aswani <priya.aswani28139@gmail.com>
2022-05-15 20:02:03 +0400

    Use tokenData for initial state of token in AuthContext

--- front-end/src/context/AuthContext.js
+++ front-end/src/context/AuthContext.js
@@ -1,12 +1,11 @@
 import React, { useState, createContext, useEffect } from "react";
 import jwt from "jwt-decode";
+import { tokenData } from "../constants";
 
 export const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
-  const [token, setToken] = useState(
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI2MjgxMTgxYWY3MDAwNDMxM2UxMDAzZWYiLCJlbWFpbCI6ImNhdEBnbWFpbC5jb20iLCJ0eXBlIjoiYWRtaW4iLCJpYXQiOjE2NTI2Mjc0ODIsImV4cCI6MTY1MzIzMjI4Mn0.89GctRUPKLJ7QyX5ryPqZT1vtw1IMyISPiu5XhW-5N8"
-  );
+  const [token, setToken] = useState(tokenData);
   const [userId, setUserId] = useState(null);
   const [isAdmin, setIsAdmin] = useState(false);
 
