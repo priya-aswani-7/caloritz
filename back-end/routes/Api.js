11f4dba3ffbdb6759f9b802e95c10e2ede726caf
Priya Aswani <priya.aswani28139@gmail.com>
2022-05-15 18:51:54 +0400

    Use authentication middleware in Api.js

--- back-end/routes/Api.js
+++ back-end/routes/Api.js
@@ -3,10 +3,95 @@ const controllers = require("../controllers");
 const FoodEntryController = require("../controllers/FoodEntryController");
 const StatisticController = require("../controllers/StatisticController");
 const UserController = require("../controllers/UserController");
+const authentication = require("../middleware/authentication");
 const router = express.Router();
 
-router.get("/:resource", (req, res) => {
+//unprotected endpoint
+//anyone can access this to login and receive an authentication token
+router.post("/user/login", (req, res) => {
+  const data = req.body;
+
+  UserController.login(data)
+    .then((data) => {
+      res.json({
+        confirmation: "success",
+        data: data,
+      });
+    })
+    .catch((error) => {
+      res.json({
+        confirmation: "fail",
+        error: error.message,
+      });
+    });
+});
+
+//unprotected endpoint
+//anyone can access this route to create a new user for themselves and receive an authentication token
+router.post("/user/register", (req, res) => {
+  const data = req.body;
+
+  UserController.post(data)
+    .then((data) => {
+      res.json({
+        confirmation: "success",
+        data: data,
+      });
+    })
+    .catch((error) => {
+      res.json({
+        confirmation: "fail",
+        error: error.message,
+      });
+    });
+});
+
+//authenticated & authorized endpoint
+//admin can access anyone's food entries
+//users can only access their own food entries
+router.get("/user/:id/foodentry", authentication, (req, res) => {
+  const id = req.params.id;
+  if (req.user.type !== "admin" && req.user.userId !== id) {
+    return res.status(403).json({
+      confirmation: "fail",
+      error:
+        "Unauthorized request. You do not have permissions to access another user's food entries.",
+    });
+  }
+  let startDate = req.query.startDate || 0;
+  let endDate = req.query.endDate || new Date().getTime();
+
+  FoodEntryController.getByUserId(id, startDate, endDate)
+    .then((data) => {
+      res.json({
+        confirmation: "success",
+        data: data,
+      });
+    })
+    .catch((error) => {
+      res.json({
+        confirmation: "fail",
+        error: error.message,
+      });
+    });
+});
+
+//authenticated & authorized endpoint
+//only admins can access users list, food entry list, statistics
+router.get("/:resource", authentication, (req, res) => {
   const resource = req.params.resource;
+  if (req.user.type !== "admin") {
+    return res.status(403).json({
+      confirmation: "fail",
+      error: `Unauthorized request. You do not have sufficient permissions to access ${
+        resource === "user"
+          ? "users list"
+          : resource === "foodentry"
+          ? "food entries list"
+          : "statistics"
+      }.`,
+    });
+  }
   const filter = req.query;
   const controller = controllers[resource];
 
@@ -33,9 +118,19 @@ router.get("/:resource", (req, res) => {
     });
 });
 
-router.get("/:resource/:id", (req, res) => {
-  const resource = req.params.resource;
+//authenticated & authorized endpoint
+//admins can view individual users
+//users can only view themselves
+router.get("/:resource/:id", authentication, (req, res) => {
   const id = req.params.id;
+  if (req.user.type !== "admin" && req.user.userId !== id) {
+    return res.status(403).json({
+      confirmation: "fail",
+      error: `Unauthorized request. You do not have sufficient permissions to access another user's details.`,
+    });
+  }
+  const resource = req.params.resource;
+
   const controller = controllers[resource];
 
   if (
@@ -64,50 +159,24 @@ router.get("/:resource/:id", (req, res) => {
     });
 });
 
-router.get("/user/:id/foodentry", (req, res) => {
-  const id = req.params.id;
-  let startDate = req.query.startDate || 0;
-  let endDate = req.query.endDate || new Date().getTime();
-
-  FoodEntryController.getByUserId(id, startDate, endDate)
-    .then((data) => {
-      res.json({
-        confirmation: "success",
-        data: data,
-      });
-    })
-    .catch((error) => {
-      res.json({
-        confirmation: "fail",
-        error: error.message,
-      });
-    });
-});
-
-router.post("/login", (req, res) => {
-  const data = req.body;
-
-  UserController.login(data)
-    .then((data) => {
-      res.json({
-        confirmation: "success",
-        data: data,
-      });
-    })
-    .catch((error) => {
-      res.json({
-        confirmation: "fail",
-        error: error.message,
-      });
-    });
-});
-
-router.post("/:resource", (req, res) => {
+//authenticated & authorized endpoint
+//admins can create food entries for any user
+//users can create food entries only for themselves
+router.post("/:resource", authentication, (req, res) => {
   const resource = req.params.resource;
   const data = req.body;
+  if (req.user.type !== "admin" && req.user.userId !== req.body.user) {
+    return res.status(403).json({
+      confirmation: "fail",
+      error: `Unauthorized request. You do not have sufficient permissions to create a food entry for another user.`,
+    });
+  }
   const controller = controllers[resource];
 
-  if (!controller || [StatisticController].includes(controller)) {
+  if (
+    !controller ||
+    [UserController, StatisticController].includes(controller)
+  ) {
     return res.json({
       confirmation: "Fail",
       message: "Invalid request.",
@@ -130,7 +199,15 @@ router.post("/:resource", (req, res) => {
     });
 });
 
-router.put("/:resource/:id", (req, res) => {
+//authenticated & authorized endpoint
+//only admins can edit food entries
+router.put("/:resource/:id", authentication, (req, res) => {
+  if (req.user.type !== "admin") {
+    return res.status(403).json({
+      confirmation: "fail",
+      error: `Unauthorized request. You do not have sufficient permissions to edit food entries.`,
+    });
+  }
   const resource = req.params.resource;
   const id = req.params.id;
   const data = req.body;
@@ -162,7 +239,15 @@ router.put("/:resource/:id", (req, res) => {
     });
 });
 
-router.delete("/:resource/:id", (req, res) => {
+//authenticated & authorized endpoint
+//only admins can delete food entries
+router.delete("/:resource/:id", authentication, (req, res) => {
+  if (req.user.type !== "admin") {
+    return res.status(403).json({
+      confirmation: "fail",
+      error: `Unauthorized request. You do not have sufficient permissions to delete food entries.`,
+    });
+  }
   const resource = req.params.resource;
   const id = req.params.id;
   const controller = controllers[resource];
