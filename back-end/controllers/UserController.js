15772c8dc20cea3494bd078eb4ae87fce87af506
Priya Aswani <priya.aswani28139@gmail.com>
2022-05-15 16:57:13 +0400

    Define post endpoint in UserController

--- back-end/controllers/UserController.js
+++ back-end/controllers/UserController.js
@@ -1,3 +1,5 @@
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
 const { User } = require("../models");
 
 module.exports = {
@@ -22,5 +24,28 @@ module.exports = {
           )
         );
     });
+  },
+
+  post: async (params) => {
+    try {
+      const password = await bcrypt.hash(params.password, 10);
+      const oldUser = await User.findOne({ email: params.email });
+      if (oldUser) {
+        throw new Error(`User with email ${params.email} already exists.`);
+      }
+
+      const user = await User.create({ ...params, password });
+      const token = await jwt.sign(
+        { userId: user._id, email: user.email, type: user.type },
+        process.env.JWT_SECRET,
+        {
+          expiresIn: "7d",
+        }
+      );
+      const data = { ...user._doc, token };
+      return data;
+    } catch (error) {
+      throw error;
+    }
   },
 };
